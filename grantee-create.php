b74f10cde82897d1a95c913eb7c9497e177e3a0f
izzdwie <90326612+izzdwie@users.noreply.github.com>
2023-11-24 10:00:46 +0800

    Initial commit

---
+++ grantee-create.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+</head>
+
+<body>
+
+    <div class="con">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Student Add
+                            <a href="index.php" class="btn btn-danger float-end">BACK</a>
+                            
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CNSC SFAU</title>
+     <link rel="icon" href="path/icon/favicon.ico" type="image/icon">
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+</body>
+
+</html>
